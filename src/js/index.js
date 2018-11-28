9e346bc09929be38eecd2f88eeb74b11b9e20768
Kyle Soeltz <skiis652@gmail.com>
2018-11-27 23:23:44 -0500

    Webpack Setup

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import {testModel} from './model/model';
+import {testView} from './view/view';
+
+testView();
+testModel();
+
+console.log(testModel);
---
+++ src/js/view/view.js
@@ -0,0 +1,3 @@
+export const testView = () => {
+	console.log('view2');
+};
